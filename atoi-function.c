a369b4b58867b564508abf6e991d957a05942982
Xinbo Wang <imoonight@gmail.com>
2017-11-07 12:30:09 -0700

    Update atoi-function.c

--- atoi-function.c
+++ atoi-function.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 
-int atoi(char s[]);
+int htoi(char s[]);
 
-main(void){
+main()
+{
+    int n;
+    char s[] = "0xFF23";
+    n = htoi(s);
+    printf("n is %d", n);
 
-     char s[100];
-     gets(s);
-     printf("interger = %d\n", atoi(s));
-    return 0;
+}
+
+int htoi(char s[])
+{
+    int i,n,j;
+    n=0;
+    if(s[0]!='0'&&(s[1]!='x'||s[1]!='X')){
+        printf("input wrong\n");
+        return 0;
     }
 
 
-
-int atoi(char s[])
-{
-    int i,n;
-    n = 0;
-    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
-        n = 10 * n + (s[i] - '0');
+    for(i=2;(s[i]>='0'&&s[i]<='9')||(s[i]>='a'&&s[i]<='f')||(s[i]>='A'&&s[i]<='F');++i){
+        if(s[i]>='0'&&s[i]<='9')
+            j=s[i]-'0';
+        else if(s[i]>='a'&&s[i]<='f')
+            j=s[i]-'a'+10;
+        else
+            j=s[i]-'A'+10;
+        n=16*n+j;
+    }
     return n;
 }
+
