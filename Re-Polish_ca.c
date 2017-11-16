25be8a5d3e853f847d6c16ab61c6b16fc49b725e
Xinbo Wang <imoonight@gmail.com>
2017-11-16 16:23:08 -0700

    Create Re-Polish_ca.c

---
+++ Re-Polish_ca.c
@@ -0,0 +1,98 @@
+//to achieve the Reverse Polish caculator
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char []);
+
+void push(double); //压入
+double pop(void);  //弹出
+
+main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0) {
+                    push(pop() / op2);
+                } else
+                    printf("error: zero divisor\n");
+
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+//push function
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+    if (sp < MAXVAL) {
+        val[sp++] = f;
+    } else
+        printf("error: stack full, cannot push %g\n", f);
+}
+
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0;
+    }
+}
+
+int getch(void);
+
+void ungetch(int);
+
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t');
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()));
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()));
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+
+}
